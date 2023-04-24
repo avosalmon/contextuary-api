f6b1b348cba5cd1823a38b0ec0a8bfa1601c70ab
Ryuta Hamasaki <ryuta.hamasaki@accredify.io>
2023-04-25 08:03:45 +0900

    add endpoint skeleton

---
+++ app/Http/Controllers/TranslationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TranslationRequest;
+
+class TranslationController extends Controller
+{
+    public function __invoke(TranslationRequest $request)
+    {
+
+    }
+}

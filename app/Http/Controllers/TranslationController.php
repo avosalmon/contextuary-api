c326ae97d8549351c77b43170793cb1b1837b2e4
Ryuta Hamasaki <ryuta.hamasaki@accredify.io>
2023-06-03 21:52:19 +0900

    remove few-shot prompt

--- app/Http/Controllers/TranslationController.php
+++ app/Http/Controllers/TranslationController.php
@@ -25,18 +25,14 @@ class TranslationController extends Controller
         $tone = $request->enum('tone', Tone::class);
         $audience = $request->input('audience');
 
-        $messages = $this->exampleMessages();
-        $messages->push(new ChatMessage(
-            Role::User,
-            (new TranslationInput(
-                word: $word,
-                context: $context,
-                inputLanguage: $inputLanguage,
-                outputLanguage: $outputLanguage,
-                tone: $tone,
-                audience: $audience,
-            ))->toPrompt(),
-        ));
+        $messages = $this->composeMessages(
+            $word,
+            $context,
+            $inputLanguage,
+            $outputLanguage,
+            $tone,
+            $audience
+        );
 
         $response = OpenAI::chat()->create([
             'model' => 'gpt-4',
@@ -55,33 +51,39 @@ class TranslationController extends Controller
     }
 
     /**
-     * Example messages for "few-shot" prompting.
-     *
      * @return Collection<ChatMessage>
      */
-    private function exampleMessages(): Collection
+    private function composeMessages(
+        string $word,
+        ?string $context,
+        string $inputLanguage,
+        string $outputLanguage,
+        Tone $tone,
+        ?string $audience,
+    ): Collection
     {
         $input = new TranslationInput(
-            word: '美味しいところを持って行ってごめんねw',
-            context: 'チームで開発していて、自分はテックリードであまりコードは書いていないのに、自分が最初のリリースボタンを押した',
-            inputLanguage: 'Japanese',
-            outputLanguage: 'English',
-            tone: Tone::Casual,
-            audience: 'Colleagues',
+            word: $word,
+            context: $context,
+            inputLanguage: $inputLanguage,
+            outputLanguage: $outputLanguage,
+            tone: $tone,
+            audience: $audience,
         );
 
         return collect([
             new ChatMessage(
                 Role::System,
-                'Act as a native speaker of both Japanese and English. Your task is to translate a word/phrase/sentence based on the context, tone, and audience.'
+                "Act as a native speaker of {$inputLanguage} and {$outputLanguage}.
+                Your task is to translate a word/phrase/sentence based on the context, tone, and audience.
+                The context and audience are optional. Ignore them if they are not provided."
             ),
             new ChatMessage(
-                Role::System,
-                $input->toPrompt(),
-                SystemUserName::ExampleUser
+                Role::User,
+                $input->toPrompt()
             ),
             new ChatMessage(
-                Role::System,
+                Role::User,
                 <<<PROMPT
                 The output should contain 3 options in the following JSON format:
                 ```
@@ -104,31 +106,7 @@ class TranslationController extends Controller
                 ]
                 ```
                 Your response should only contain the JSON object as I will programmatically parse it.
-                PROMPT,
-                SystemUserName::ExampleUser
-            ),
-            new ChatMessage(
-                Role::System,
-                <<<RESPONSE
-                [
-                    {
-                        "translation": "Sorry for stealing the limelight, haha!",
-                        "explanation": "The phrase '美味しいところを持って行ってごめんねw' literally translates to 'Sorry for taking the delicious part'. However, in the context provided, it's a metaphorical way of saying that the speaker has taken the best or most rewarding part (i.e., pressing the release button) despite not having contributed as much in terms of coding.",
-                        "example": "I know you guys did most of the coding, but there I went, pushing the release button first. Sorry for stealing the limelight, haha!"
-                    },
-                    {
-                        "translation": "My bad for hogging the glory moment, lol!",
-                        "explanation": "The '美味しいところを持って行ってごめんねw' phrase can also imply the speaker 'hogging' or taking more than their fair share, which in this context would be the glory of the launch despite not being heavily involved in the development process.",
-                        "example": "I'm not the one who did most of the coding, yet here I am pushing the release button. My bad for hogging the glory moment, lol!"
-                    },
-                    {
-                        "translation": "Apologies for taking the best part for myself, haha!",
-                        "explanation": "In this context, '美味しいところを持って行ってごめんねw' is equivalent to the speaker apologizing for taking the 'best part' for themselves, which is pressing the release button, despite not writing much of the code.",
-                        "example": "Even though I haven't written much of the code, I'm the one who got to press the release button. Apologies for taking the best part for myself, haha!"
-                    }
-                ]
-                RESPONSE,
-                SystemUserName::ExampleAssistant
+                PROMPT
             ),
         ]);
     }

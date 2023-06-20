097e080d328dbb23d2613c14767469408212ecc8
Ryuta Hamasaki <ryuta.hamasaki@accredify.io>
2023-06-20 23:58:42 +0900

    Tranlation API (#1)
    Co-authored-by: avosalmon <avosalmon@users.noreply.github.com>

---
+++ app/DataObjects/TranslationInput.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataObjects;
+
+use App\Enums\Tone;
+
+readonly class TranslationInput
+{
+    public function __construct(
+        public string $word,
+        public string $context,
+        public string $inputLanguage,
+        public string $outputLanguage,
+        public Tone $tone,
+        public string $audience
+    ) {
+    }
+
+    public function toPrompt(): string
+    {
+        return <<<PROMPT
+Input: {$this->word}
+Context: {$this->context}
+From language: {$this->inputLanguage}
+To language: {$this->outputLanguage}
+Tone: {$this->tone->name}
+Audience: {$this->audience}
+PROMPT;
+    }
+}
---
+++ app/Http/Controllers/TranslationController.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTranslationRequest;
+use App\Http\Resources\Translation;
+use OpenAI\Laravel\Facades\OpenAI;
+
+class TranslationController extends Controller
+{
+    public function store(StoreTranslationRequest $request): Translation
+    {
+        $response = OpenAI::chat()->create([
+            'model' => 'gpt-4-0613',
+            'messages' => [
+                ['role' => 'user', 'content' => $request->toPrompt()],
+            ],
+            'functions' => [
+                $request->toFunction(),
+            ],
+            'function_call' => [
+                'name' => 'store_translation',
+            ],
+            'temperature' => 0,
+            // 'max_tokens' => xxx,
+        ]);
+
+        $arguments = $response->choices[0]->message->functionCall->arguments;
+        $json = json_decode($arguments, associative: true);
+
+        // TODO: Store the translation in the database
+
+        return new Translation($json);
+    }
+}

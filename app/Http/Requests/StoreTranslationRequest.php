04794751431ce701dc1296119a0efbaf99f92c2c
Ryuta Hamasaki <ryuta.hamasaki@accredify.io>
2023-06-21 09:41:28 +0900

    update the translation prompt

--- app/Http/Requests/StoreTranslationRequest.php
+++ app/Http/Requests/StoreTranslationRequest.php
@@ -46,20 +46,23 @@ class StoreTranslationRequest extends FormRequest
         $tone = $this->enum('tone', Tone::class) ?? Tone::Neutral;
         $audience = $this->input('audience');
 
-        $prompt = "Translate the following input: \"{$input}\" in {$outputLanguage}";
-        $prompt .= $context ? ", which is used in this context: \"{$context}\".\n" : ".\n";
-        $prompt .= "The intended tone is \"{$tone->value}\"";
-        $prompt .= $audience ? " and the audience is \"{$audience}\".\n" : ".\n";
-        $prompt .= "Provide the following in your response.\n\n";
-        $prompt .= "Translation: the translation of the word/phrase in the given context and tone. Make it sound natural to native English speakers rather than just literally translating the input.\n";
+        $prompt = "Your task is to perform the following action(s):\n";
+        $prompt .= "1. Translate the following {$inputLanguage} text to {$outputLanguage} in the given context and tone. ";
+        $prompt .= "Make it sound natural to native {$outputLanguage} speakers rather than just literally translating the text.\n";
 
         if ($this->boolean('requires_explaination')) {
-            $prompt .= "Explanation: the explanation of the translation in {$inputLanguage}, touching on the {$outputLanguage} words/phrases used in the translation. Keep any {$outputLanguage} phrases you mention in the explanation in their original {$outputLanguage} form.\n";
+            $prompt .= "2. Explain the translation in {$inputLanguage}, touching on the {$outputLanguage} words/phrases used in the translation. Keep any {$outputLanguage} phrases you mention in the explanation in their original {$outputLanguage} form.\n";
         }
 
         if ($this->boolean('requires_example')) {
-            $prompt .= "Example: an example conversation between 2 persons in {$outputLanguage} using the translation";
+            $prompt .= "3. Provide an example conversation between 2 persons in {$outputLanguage} using the translation\n";
         }
+
+        $prompt .= "\n";
+        $prompt .= "Text: {$input}\n";
+        $prompt .= $context ? "Context: {$context}\n" : '';
+        $prompt .= "Tone: {$tone->value}\n";
+        $prompt .= $audience ? "Audience: {$audience}" : '';
 
         return $prompt;
     }
